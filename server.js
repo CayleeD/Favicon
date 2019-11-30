3d43b1df568eae02b65f1b0d529f1c26557f907f
Caylee <CayleeDias@gmail.com>
2019-11-30 16:20:27 -0600

    Added Express server code to dev's server.js file.

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
-console.log("This is a test.");
-console.log("This is a second test."); 
+const express = require("express"); // Import the Express Package
+const server = express(); // Create the server.
+
+// Handle requests to the root of the api: The / Route
+server.get('/', (req, res) => {
+    res.send("Hello from Express.");
+});
+
+// Watch for connections on port 5000
+server.listen(5000, () =>
+    console.log("Server running on http://localhost:500")
+);
